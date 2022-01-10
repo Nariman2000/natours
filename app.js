df226112deaa5ba72570b83b3914e27282270641
Nariman Naghavi <nariman.naghavi78@gmail.com>
2022-01-10 17:00:04 +0330

    Added a comment

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const { AppError } = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const setHeaderErr = require('./utils/setHeaderError');
 
+// START EXPRESS APP
 const app = express();
 
 // TEMPLATE ENGINE
